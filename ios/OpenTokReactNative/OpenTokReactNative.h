591b75d8a5877ac8e6628864dcc4f74b3e4e2d84
Manik <msach22@gmail.com>
2018-05-16 18:12:17 -0700

    create ios lib instead of just files so we can use react-native link to install

---
+++ ios/OpenTokReactNative/OpenTokReactNative.h
@@ -0,0 +1,13 @@
+//
+//  OpenTokReactNative.h
+//  OpenTokReactNative
+//
+//  Created by Manik Sachdeva on 5/16/18.
+//  Copyright © 2018 TokBox Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenTokReactNative : NSObject
+
+@end
