27a21d2d9be4d4104168c5fa5752b7989886733b
Vishnu Ramineni <vishnu.ramineni.05@gmail.com>
2020-10-28 12:05:27 +0530

    Fixed android app crash with API level 29

--- android/src/main/java/com/opentokreactnative/OTSubscriberViewManager.java
+++ android/src/main/java/com/opentokreactnative/OTSubscriberViewManager.java
@@ -26,8 +26,9 @@ public class OTSubscriberViewManager extends ViewGroupManager<OTSubscriberLayout
 
     @ReactProp(name = "streamId")
     public void setStreamId(OTSubscriberLayout view, String streamId) {
-
+        if(streamId!=null){
         view.createSubscriberView(streamId);
+        }
     }
 
 }
