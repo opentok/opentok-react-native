7b1c40a9a1176fcb0aae48867de96bf100ccd70c
vishnu-apollo247 <vishnu.r@apollo247.org>
2020-11-08 13:01:22 +0530

    formatted the code

--- android/src/main/java/com/opentokreactnative/OTSubscriberViewManager.java
+++ android/src/main/java/com/opentokreactnative/OTSubscriberViewManager.java
@@ -26,8 +26,8 @@ public class OTSubscriberViewManager extends ViewGroupManager<OTSubscriberLayout
 
     @ReactProp(name = "streamId")
     public void setStreamId(OTSubscriberLayout view, String streamId) {
-        if(streamId != null) {
-        view.createSubscriberView(streamId);
+        if (streamId != null) {
+            view.createSubscriberView(streamId);
         }
     }
 
