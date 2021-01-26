18204e2a559f0f311a6de308f10f21ccfa5f5aac
Enrico Portolan <enrico.portolan@vonage.com>
2021-01-26 16:05:16 +0000

    [OTSubscriber] - Add preferred resolution & frameRate (#434)
    * - add subscriber helper to sanitize res and frameRate
    - add resolution and FR on the didUpdate Hook
    - OTSEssionManager.java support for preferred Res and FrameRate
    
    * - fix faulty merge
    
    * - add VideoUtils import
    
    * - add support PreferredRes and FrameRate to Android
    - Fixed OTSubscriber Helper
    
    * - add sanitizePreferredFrameRate
    - add sanitizePreferredResolution
    
    * - fix OTSubscriber didUpdate webhook
    
    * - fix setPreferredFR in OTSessionManager.m
    - fix max resolution and frameRate
    
    * - fix resolution value
    
    * - README & CHANGELOG updated
    - bump package.json version
    
    * - fix OTSubcriber indentation
    
    * - Fix OTSubscriberHelper indentation
    
    * - adjust supported PreferredRes
    
    * - change resolution parameter from string to object
    
    * - fix typo in OTSubscriber

--- android/src/main/java/com/opentokreactnative/OTSessionManager.java
+++ android/src/main/java/com/opentokreactnative/OTSessionManager.java
@@ -6,8 +6,9 @@ package com.opentokreactnative;
 
 import android.util.Log;
 import android.widget.FrameLayout;
-import android.support.annotation.Nullable;
 import android.view.View;
+
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
@@ -29,6 +30,7 @@ import com.opentok.android.Stream;
 import com.opentok.android.OpentokError;
 import com.opentok.android.Subscriber;
 import com.opentok.android.SubscriberKit;
+import com.opentok.android.VideoUtils;
 import com.opentokreactnative.utils.EventUtils;
 import com.opentokreactnative.utils.Utils;
 
@@ -218,6 +220,18 @@ public class OTSessionManager extends ReactContextBaseJavaModule
         mSubscriber.setStreamListener(this);
         mSubscriber.setSubscribeToAudio(properties.getBoolean("subscribeToAudio"));
         mSubscriber.setSubscribeToVideo(properties.getBoolean("subscribeToVideo"));
+        if (properties.hasKey("preferredFrameRate")) {
+            mSubscriber.setPreferredFrameRate((float) properties.getDouble("preferredFrameRate"));
+        }
+        if (properties.hasKey("preferredResolution")
+                && properties.getMap("preferredResolution").hasKey("width")
+                && properties.getMap("preferredResolution").hasKey("height")) {
+            ReadableMap preferredResolution = properties.getMap("preferredResolution");
+            VideoUtils.Size resolution = new VideoUtils.Size(
+                    preferredResolution.getInt("width"),
+                    preferredResolution.getInt("height"));
+            mSubscriber.setPreferredResolution(resolution);
+        }
         mSubscribers.put(streamId, mSubscriber);
         if (mSession != null) {
             mSession.subscribe(mSubscriber);
@@ -302,6 +316,34 @@ public class OTSessionManager extends ReactContextBaseJavaModule
         Subscriber mSubscriber = mSubscribers.get(streamId);
         if (mSubscriber != null) {
             mSubscriber.setSubscribeToVideo(subscribeToVideo);
+        }
+    }
+
+    @ReactMethod
+    public void setPreferredResolution(String streamId, ReadableMap resolution) {
+
+        ConcurrentHashMap<String, Subscriber> mSubscribers = sharedState.getSubscribers();
+        Subscriber mSubscriber = mSubscribers.get(streamId);
+        if (mSubscriber != null ) {
+            if (resolution.hasKey("width")
+                    && resolution.hasKey("height")) {
+                VideoUtils.Size preferredResolution = new VideoUtils.Size(
+                        resolution.getInt("width"),
+                        resolution.getInt("height"));
+                mSubscriber.setPreferredResolution(preferredResolution);
+            } else {
+                mSubscriber.setPreferredResolution(SubscriberKit.NO_PREFERRED_RESOLUTION);
+            }
+        }
+    }
+
+    @ReactMethod
+    public void setPreferredFrameRate(String streamId, Float frameRate) {
+
+        ConcurrentHashMap<String, Subscriber> mSubscribers = sharedState.getSubscribers();
+        Subscriber mSubscriber = mSubscribers.get(streamId);
+        if (mSubscriber != null) {
+            mSubscriber.setPreferredFrameRate(frameRate);
         }
     }
 
