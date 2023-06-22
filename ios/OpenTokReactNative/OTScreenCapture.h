39e761ba9df9d5b44d3d289aad65f2f431116ab1
Jeronimo Valli <Jeronimo.Valli@Siriusxm.com>
2023-06-22 21:03:51 +0200

    VIDCS-974: React Native- IOS: App crash

--- ios/OpenTokReactNative/OTScreenCapture.h
+++ ios/OpenTokReactNative/OTScreenCapture.h
@@ -10,13 +10,6 @@
 
 @protocol OTVideoCapture;
 
-// defines for image scaling
-// From https://bugs.chromium.org/p/webrtc/issues/detail?id=4643#c7 :
-// Don't send any image larger than 1280px on either edge. Additionally, don't
-// send any image with dimensions %16 != 0
-#define MAX_EDGE_SIZE_LIMIT 1280.0f
-#define EDGE_DIMENSION_COMMON_FACTOR 16.0f
-
 /**
  * Periodically sends video frames to an OpenTok Publisher by rendering the
  * CALayer for a UIView.
@@ -30,9 +23,5 @@
  */
 - (instancetype)initWithView:(UIView*)view;
 
-// private: declared here for testing scaling & padding function
-+ (void)dimensionsForInputSize:(CGSize)input
-                 containerSize:(CGSize*)destContainerSize
-                      drawRect:(CGRect*)destDrawRect;
 
 @end
