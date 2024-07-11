34c152e47aa7ebd88106af28f79aaf84276fac88
Marina Serrano Montes <marina.serranomontes@vonage.com>
2024-07-11 19:58:28 +0200

    Fixing referneces to media lib comments

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -512,7 +512,7 @@ declare module "opentok-react-native" {
     /**
      * Sets video transformers for the publisher (or clears them if passed an empty array).
      * To use this method, add `pod 'VonageClientSDKVideoTransformers'` to your
-     * Podfile and add `implementation "com.vonage:client-sdk-video-transformers:2.27.2"`
+     * Podfile and add `implementation "com.vonage:client-sdk-video-transformers:2.28.0"`
      * to your your app/build.gradle file.
      */
     setVideoTransformers: (transformers: Array<{
