b2349d3f2fb380d34c4356a3c0dd89d6cb3be889
Jeff Swartz <jeff.swartz@vonage.com>
2025-02-20 15:50:07 -0800

    register native components in sample app

--- example/android/app/src/main/java/opentokreactnative/example/MainApplication.kt
+++ example/android/app/src/main/java/opentokreactnative/example/MainApplication.kt
@@ -11,6 +11,8 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.opentokreactnative.OTSubscriberViewNativePackage
+import com.opentokreactnative.OpentokReactNativePackage;
 
 class MainApplication : Application(), ReactApplication {
 
@@ -18,8 +20,8 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+              add(OTSubscriberViewNativePackage())
+              add(OpentokReactNativePackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
