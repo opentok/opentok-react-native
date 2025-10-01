0f04edb3af3fcee61878877b354eff9013dd8184
Jeff Swartz <jeff.swartz@vonage.com>
2025-10-01 00:39:03 -0700

    Start upgrading to react-native 0.81

--- example/android/app/src/main/java/opentokreactnative/example/MainApplication.kt
+++ example/android/app/src/main/java/opentokreactnative/example/MainApplication.kt
@@ -4,16 +4,11 @@ import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
-import com.facebook.soloader.SoLoader
-import com.opentokreactnative.OTRNSubscriberPackage
-import com.opentokreactnative.OTRNPublisherPackage
-import com.opentokreactnative.OpentokReactNativePackage;
 
 class MainApplication : Application(), ReactApplication {
 
@@ -21,9 +16,8 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              add(OTRNPublisherPackage())
-              add(OTRNSubscriberPackage())
-              add(OpentokReactNativePackage())
+              // Packages that cannot be autolinked yet can be added manually here, for example:
+              // add(MyReactNativePackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
@@ -39,10 +33,6 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
+    loadReactNative(this)
   }
 }
