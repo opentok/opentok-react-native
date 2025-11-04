58528cc557bd1240e08ad543debdb37c1d58ff61
Mike-Zhylevych <mike.zhylevych@vonage.com>
2025-11-04 14:26:12 +0100

    test new rn

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,18 +1,13 @@
 const path = require('path');
 const { getDefaultConfig } = require('@react-native/metro-config');
-const { getConfig } = require('react-native-builder-bob/metro-config');
-const pkg = require('../package.json');
 
-const root = path.resolve(__dirname, '..');
-
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-module.exports = getConfig(getDefaultConfig(__dirname), {
-  root,
-  pkg,
-  project: __dirname,
-});
+module.exports = {
+  ...getDefaultConfig(__dirname),
+  watchFolders: [path.resolve(__dirname, '..')],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, '../node_modules'),
+    ],
+  },
+};
