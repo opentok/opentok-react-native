f0199311a44524684dd698e9afad8f93f1f86a37
Mike-Zhylevych <mike.zhylevych@vonage.com>
2025-11-10 14:41:29 +0100

    feat(component): added package.json and eslint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   ignores: ['node_modules/', 'example/ios/Pods', 'lib/'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+  },
 };
