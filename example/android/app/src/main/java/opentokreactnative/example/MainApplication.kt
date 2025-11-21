81832f44dff750b3fe27644a87f42774c885bfcd
Mike-Zhylevych <mike.zhylevych@vonage.com>
2025-11-21 12:26:57 +0100

    changes

--- example/android/app/src/main/java/opentokreactnative/example/MainApplication.kt
+++ example/android/app/src/main/java/opentokreactnative/example/MainApplication.kt
@@ -27,7 +27,9 @@ class MainApplication : Application(), ReactApplication {
             }
 
         override fun getJSMainModuleName(): String = "index"
+
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
